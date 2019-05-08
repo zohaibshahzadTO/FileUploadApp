d71d4b454526e1e56e591dd359463844b93e2b86
Zohaib Shahzad <zohaibshahzad.to@gmail.com>
2019-05-08 19:36:43 -0400

    Project Setup + Mass Update
    - Imported middleware
    - Progress w/ backend for file upload component

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const App = () => (
+  <div className="container mt-4">
+    <h4 className="display-4 text-center mb-4">
+      <i className="fab fa-react" /> File Upload App
+    </h4>
+  </div>
+);
+
+export default App;
